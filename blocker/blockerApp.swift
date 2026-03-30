3a20dfefc6e245f415f740e935005cca22bb2f4f
John <john.thom.gray@gmail.com>
2026-03-30 16:45:32 -0400

    Initial Commit

---
+++ blocker/blockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  blockerApp.swift
+//  blocker
+//
+//  Created by John Gray on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct blockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
